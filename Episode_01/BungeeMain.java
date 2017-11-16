97ea9c613b17244e288ba381bde550ecfd2d50c6
{TheSourceCode} <The-SourceCode@users.noreply.github.com>
2017-11-15 19:29:06 -0500

    1

---
+++ Episode_01/BungeeMain.java
@@ -0,0 +1,11 @@
+package me.tsccoding;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BungeeMain extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+        getLogger().info("has loaded!");
+    }
+}
